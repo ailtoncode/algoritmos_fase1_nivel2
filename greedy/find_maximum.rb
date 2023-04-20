4f35ed0aa64d4f72d3413c23ba5a36df684e40e8
ailtoncode <contato.sc17@gmail.com>
2023-04-20 10:47:03 -0300

    refactor: Removal of unnecessary lines of code

--- greedy/find_maximum.rb
+++ greedy/find_maximum.rb
@@ -2,33 +2,24 @@ class FindMaximum
     def find(stacks)
         all = sum_all(stacks)
         total = !all.empty? ? all.inject(:+) : 0
-        equal_value = 0
-
         total.times do
             sum = sum_all(stacks)
             previous = {"index" => 0, "max_sum" => sum[0]}
-            compare = sum[0]
             equal = true
             for i in 1..(sum.length-1) do
                 if sum[i] > previous["max_sum"]
                     previous["index"] = i
                     previous["max_sum"] = sum[i]
                 end
-                if sum[i] != sum[i-1]
-                    equal = false
-                end
+                equal = sum[i] != sum[i-1] ? false : equal
             end
             if !equal
                 stacks[previous["index"]].shift
-                if stacks[previous["index"]].empty?
-                    stacks[previous["index"]] << 0
-                end
-            elsif equal
-                equal_value = previous["max_sum"]
-                break
+                stacks[previous["index"]] << 0 if stacks[previous["index"]].empty?
+            else
+                return previous["max_sum"]
             end
         end
-        equal_value
     end
 
     private
@@ -56,9 +47,9 @@ stacks = [
 ]
 =end
 stacks = [
-    [3, 2, 1, 1 , 4],
-    [4, 6, 2],
-    [2, 6, 1, 1]
+    [3, 2, 1, 1 , 1],
+    [4, 3, 2],
+    [2, 5, 4, 1]
 ]
 
 find_maximum = FindMaximum.new
